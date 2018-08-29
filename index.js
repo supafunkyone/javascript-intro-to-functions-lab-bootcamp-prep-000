82d4cdf4a76a7581f1640386f390993bae30f5c5
alexsoleleris@gmail.com <alexsoleleris@gmail.com>
2018-08-29 19:04:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,19 +7,5 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log("hello".toUpperCase)
+  console.log('hello'.toUpperCase)
 }
-
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase
- 
-mixedCase.toUpperCase() === mixedCase
