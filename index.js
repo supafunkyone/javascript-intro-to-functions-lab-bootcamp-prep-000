048c73e992dad0605bbfa1c8d470acf9b6773075
alexsoleleris@gmail.com <alexsoleleris@gmail.com>
2018-08-29 19:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function whisper(string) {
 function logShout(string) {
   console.log("HELLO")
 }
+
+function logWhisper(string) {
+  console.log("WHISPER")
+}
+
+function sayHiToGrandma(string) {
+  return string
+}
